c0322f36f7f600388c8f6a9b71adea0091a014fd
Daniel Kolman <daniel@kolman.cz>
2015-02-05 08:07:49 +0100

    Extract brew, sync instead of symlinks

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,78 +1,17 @@
 #!/usr/bin/env bash
+chmod +x brew.sh
+./brew.sh
+
+# dotfiles
+DOTFILES_DIR=~/dotfiles
+git clone https://github.com/kolman/dotfiles.git $DOTFILES_DIR
+
+cd $DOTFILES_DIR
+chmod +x sync.sh
+chmod +x brew.sh
+./sync.sh
+
 cd ~
-
-# Check for Homebrew,
-# Install if we don't have it
-if test ! $(which brew); then
-  echo "Installing homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
-
-# Update homebrew recipes
-brew update
-
-# Install GNU core utilities (those that come with OS X are outdated)
-#brew install coreutils
-
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
-# brew install findutils
-
-# Install Bash 4
-brew install bash
-
-# Install more recent versions of some OS X tools
-#brew tap homebrew/dupes
-#brew install homebrew/dupes/grep
-
-binaries=(
-  rename
-  python
-  node
-  git
-  wget
-  wdiff
-  dos2unix
-  fish
-  macvim
-  ivy
-  leiningen
-  the_silver_searcher
-)
-
-echo "installing binaries..."
-brew install ${binaries[@]}
-
-brew cleanup
-
-brew install caskroom/cask/brew-cask
-brew tap caskroom/versions
-
-# Apps
-apps=(
-  java
-  iterm2
-  virtualbox
-  sourcetree
-  tunnelblick
-  disk-inventory-x
-  selfcontrol
-  google-chrome
-  firefox
-  evernote
-  skitch
-  skype
-  hipchat
-  mplayerx
-  spotify
-)
-
-# Link apps to Alfred
-#brew cask alfred link
-
-# Install apps to /Applications
-# Default is: /Users/$user/Applications
-echo "installing apps..."
-brew cask install --appdir="/Applications" ${apps[@]}
 
 # oh-my-fish
 curl -L https://github.com/bpinto/oh-my-fish/raw/master/tools/install.fish | fish
@@ -89,12 +28,6 @@ if [ ! -f $GITHUB_KEY ]; then
   echo "\n" >> $SSH_CONFIG
 fi
 
-# dotfiles
-DOTFILES_DIR=~/dotfiles
-git clone https://github.com/kolman/dotfiles.git $DOTFILES_DIR
-
-# dotfiles - Git
-ln -s $DOTFILES_DIR/.gitconfig ~/.gitconfig
 
 # fish
 ## add fish to list of shells
---
+++ brew.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+# Check for Homebrew,
+# Install if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update homebrew recipes
+brew update
+
+# Install Bash 4
+brew install bash
+
+binaries=(
+  rename
+  python
+  node
+  git
+  wget
+  wdiff
+  dos2unix
+  fish
+  macvim
+  ivy
+  leiningen
+  the_silver_searcher
+)
+
+echo "installing binaries..."
+brew install ${binaries[@]}
+
+brew cleanup
+
+brew install caskroom/cask/brew-cask
+brew tap caskroom/versions
+
+# Apps
+apps=(
+  java
+  iterm2
+  virtualbox
+  sourcetree
+  tunnelblick
+  disk-inventory-x
+  selfcontrol
+  google-chrome
+  firefox
+  evernote
+  skitch
+  skype
+  hipchat
+  mplayerx
+  spotify
+)
+
+# Install apps to /Applications
+# Default is: /Users/$user/Applications
+echo "installing apps..."
+brew cask install --appdir="/Applications" ${apps[@]}
+
